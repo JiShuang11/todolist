2f7e8c612abaa297ec6f94200118ec3713528b95
licaiting <Shuang9T@gmail.com>
2026-01-21 20:33:27 +0800

    路由设计

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div class="todo-item">
+    <input type="checkbox" />
+    <span>{{ title }}</span>
+  </div>
+</template>
+
+<style scoped>
+.todo-item {
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        { path: '/todo', component: () => import('@/views/todo/index.vue') },
+        { path: '/form', component: () => import('@/views/form/index.vue') },
+        {
+            path: '/profile',
+            component: () => import('@/views/profile/index.vue'),
+        },
+    ],
+});
 
-export default router
+export default router;
