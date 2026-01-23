0636d704bd7171d63962746b6e6db1a499169d90
licaiting <Shuang9T@gmail.com>
2026-01-23 15:18:06 +0800

    feat(用户信息): 添加个人信息表单以收集用户数据
    引入表单组件，包含姓名输入框、地区选择下拉菜单和性别单选按钮，用于收集和提交用户信息。

--- src/views/UserInfo/index.vue
+++ src/views/UserInfo/index.vue
@@ -1,13 +1,45 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
 import MainLayout from '@/components/MainLayout.vue';
+const form = reactive({
+    name: '',
+    region: '',
+    resource: '',
+});
+
+const onSubmit = () => {
+    console.log('submit!');
+};
 </script>
 
 <template>
     <MainLayout>
         <template #user>
-            <div style="padding: 20px;">
-                <h2>个人中心</h2>
-                <p>这里是个人信息页面</p>
+            <div style="padding: 20px">
+                <el-form
+                    :model="form"
+                    label-width="auto"
+                    style="max-width: 600px"
+                >
+                    <el-form-item label="姓名">
+                        <el-input v-model="form.name" />
+                    </el-form-item>
+                    <el-form-item label="地区">
+                        <el-select
+                            v-model="form.region"
+                            placeholder="请选择你所在的地区"
+                        >
+                            <el-option label="上海" value="shanghai" />
+                            <el-option label="北京" value="beijing" />
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="性别">
+                        <el-radio-group v-model="form.resource">
+                            <el-radio value="Sponsor">男</el-radio>
+                            <el-radio value="Venue">女</el-radio>
+                        </el-radio-group>
+                    </el-form-item>
+                </el-form>
             </div>
         </template>
     </MainLayout>
